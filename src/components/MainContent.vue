7b4abb1a33064740a748ad9a4abe392bf26b40f4
AlessioDPT <alessio.recos@gmail.com>
2021-07-08 17:52:15 +0200

    ADD- COMP music card

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,13 +1,48 @@
 <template>
-  <main></main>
+  <main>
+      <div class="box p-5 text-center">
+
+          <div class="row d-flex justify-content-center">
+
+              <div class="col-2 m-3" v-for="(item, index) in dataMusic" :key="index">
+
+              <MusicCard :info="item"/>
+
+              </div>
+
+
+          </div>
+
+      </div>
+  </main>
 </template>
 
 <script>
+import axios from 'axios'
+import MusicCard from '@/components/MusicCard.vue'
 export default {
-
+    name: "MainContent",
+    components:{
+        MusicCard
+    },
+    data(){
+        return{
+            musicItems : "https://flynn.boolean.careers/exercises/api/array/music",
+            dataMusic: ""
+        }
+    },
+    created(){
+        this.getDataApi()
+    },
+    methods: {
+        getDataApi(){
+            axios
+                .get(this.musicItems)
+                .then(result =>{
+                    this.dataMusic= result.data.response
+                    console.log(this.dataMusic)
+            })
+        }
+    }
 }
 </script>
-
-<style>
-
-</style>
---
+++ src/components/MusicCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+      <img :src="info.poster" :alt="info.title">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MusicCard",
+    props: ["info"]
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/commons.scss";
+    .card {
+        background-color: $bgcSpotLight;
+        width: 100%;
+        height: 360px;
+    }
+
+
+</style>
+
+
+// "poster": "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+//             "title": "New Jersey",
+//             "author": "Bon Jovi",
+//             "genre": "Rock",
+//             "year": "1988"
+// ciao
