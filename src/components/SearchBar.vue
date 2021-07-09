a7b9cadb350845822f5544c29f59fba0a872d82d
AlessioDPT <alessio.recos@gmail.com>
2021-07-09 16:35:04 +0200

    ADD- cambio selezione in SEARCHBAR COMP

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="search_bar mx-5">
-    <select class="form-select" aria-label="Default select example">
-    <option selected>Open this select menu</option>
-    <option value="1">Pop</option>
-    <option value="2">jazz</option>
-    <option value="3">Metal</option>
-    <option value="4">Rock</option>
+    <select class="form-select" aria-label="Default select example" selected v-model="selectedGen" @change="changing()">
+    <option value="Pop">Pop</option>
+    <option value="Jazz">jazz</option>
+    <option value="Metal">Metal</option>
+    <option value="Rock">Rock</option>
 </select>
   </div>
 </template>
 
 <script>
 export default {
-    name:"SearchBar"
+    name: "SearchBar",
+    data(){
+        return {
+             selectedGen: "",
+        }
+    },
+    methods: {
+        changing(){
+            console.log(this.selectedGen)
+            
+        }
+    }
+    
 
 }
 </script>
